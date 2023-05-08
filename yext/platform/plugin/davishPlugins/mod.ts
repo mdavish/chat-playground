ebea16bc5c7acb44dc52c9491e993a33e30563f3
Maxwell Davish <mdavish@yext.com>
2023-05-08 15:50:15 -0400

    converting weather fetching into serverless function for testing

--- yext/platform/plugin/davishPlugins/mod.ts
+++ yext/platform/plugin/davishPlugins/mod.ts
@@ -1,3 +1,33 @@
+import { z } from "https://deno.land/x/zod/mod.ts";
+
+const ChatFunctionPayloadSchema = z.object({
+  messages: z.array(
+    z.object({
+      text: z.string(),
+      // TODO: Add source enum once it's fixed in the back-end
+      timestamp: z.string(),
+    })
+  ),
+  notes: z
+    .object({
+      currentGoal: z.string().optional(),
+      currentStepIndices: z.array(z.number()).optional(),
+      searchQuery: z.string().optional(),
+      queryResult: z.any(),
+      collectedData: z.record(z.string()).optional(),
+      goalFirstMsgIndex: z.number().optional(),
+    })
+    .optional(),
+});
+
+export type ChatFunctionPayload = z.infer<typeof ChatFunctionPayloadSchema>;
+
+export type ChatFunctionReturn = Partial<ChatFunctionPayload["notes"]>;
+
+export type ChatFunction = (
+  payload: ChatFunctionPayload
+) => Promise<ChatFunctionReturn>;
+
 export const getBrooklynWeather = async () => {
   const params = {
     // Brooklyn NY
@@ -10,4 +40,33 @@ export const getBrooklynWeather = async () => {
   const response = await fetch(url);
   const data = await response.json();
   return data;
+};
+
+export const geocodeCity = async (city: string) => {
+  const params = {
+    city,
+    apiKey: "1ccf45d1a1c47111ace6f290a4ee2685",
+  };
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${params.city}&appid=${params.apiKey}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
+
+// Note: In the future this can be a simple REST API instruction
+// But right now we don't support dynamic URLs based on collected data
+export const getWeather: ChatFunction = async ({ notes }) => {
+  const parsedCity = z.string().safeParse(notes?.collectedData?.city);
+  if (!parsedCity.success) {
+    throw new Error(
+      "No city was collected. Make sure to add a `city` field first."
+    );
+  }
+  const city = parsedCity.data;
+  const data = await geocodeCity(city);
+  // The geocode response actually contains the weather data
+  // So no need to make a follow up request
+  return {
+    queryResult: data,
+  };
 };
--- yext/platform/plugin/davishPlugins/test.ts
+++ yext/platform/plugin/davishPlugins/test.ts
@@ -1,6 +1,37 @@
-import { getBrooklynWeather } from "./mod.ts";
+import { getBrooklynWeather, geocodeCity, getWeather } from "./mod.ts";
+import type { ChatFunctionPayload } from "./mod.ts";
 
 Deno.test("Get the Weather in Brooklyn", async () => {
   const data = await getBrooklynWeather();
+  console.log({ data });
+});
+
+Deno.test("Geocode a few cities", async () => {
+  const cityNames = [
+    "Brooklyn",
+    "New York",
+    "San Francisco",
+    "Los Angeles",
+    "Chicago",
+    "Houston",
+    "Philadelphia",
+    "Phoenix",
+  ];
+  for (const city of cityNames) {
+    const data = await geocodeCity(city);
+    console.log({ data });
+  }
+});
+
+Deno.test("Respond to a chat message with the weather", async () => {
+  const fakePayload: ChatFunctionPayload = {
+    messages: [],
+    notes: {
+      collectedData: {
+        city: "Brooklyn",
+      },
+    },
+  };
+  const data = await getWeather(fakePayload);
   console.log({ data });
 });
